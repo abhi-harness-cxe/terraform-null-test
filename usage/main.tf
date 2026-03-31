b60e209300a5bf2e34335eb53654d185c8bce24e
abhi-harness-cxe <abhishek.sahu@harness.io>
2026-03-31 12:13:54 -0700

    Add message and output to test-null-module
    Added a message to the test-null-module and an output for it.

--- usage/main.tf
+++ usage/main.tf
@@ -1,4 +1,10 @@
 module "test-null-module" {
   source  = "app.harness.io/B91tP-WFQKiWjJ_i0OoZ1g/test-null-module/test"
   version = "v1.0.0"
+  message = "Hello from Terraform Module 🚀"
 }
+
+output "module_message" {
+  value = module.test-null-module.echoed_message
+}
+
