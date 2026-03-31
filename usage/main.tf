305f4ef1b15a11ed9c9ef947025dc31804ef0187
abhi-harness-cxe <abhishek.sahu@harness.io>
2026-03-31 12:16:20 -0700

    Use variable for module message in main.tf
    Replaced static message with variable output_message.

--- usage/main.tf
+++ usage/main.tf
@@ -1,10 +1,15 @@
 module "test-null-module" {
   source  = "app.harness.io/B91tP-WFQKiWjJ_i0OoZ1g/test-null-module/test"
   version = "v1.0.0"
-  message = "Hello from Terraform Module 🚀"
+  message = var.output_message
 }
 
 output "module_message" {
   value = module.test-null-module.echoed_message
 }
+
+variable "output_message" {
+  default = "Hello World"
+}
+
 
